93e7c831b23249a0f122e77f349b5851f6fc0b28
NITA-san <ysnitta@gmail.com>
2021-02-17 22:47:22 +0900

    Initial commit

---
+++ demo/src/main/java/com/example/demo/TestServlet.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(name = "search", urlPatterns = { "/search" })
+public class TestServlet extends HttpServlet{
+    
+}
